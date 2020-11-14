7cf822babff50f88ea1dfea09d0eae18d18e1c36
qitianliang <qitianliang@outlook.com>
2020-11-14 17:09:15 +0800

    cd home

--- pyenvs.sh
+++ pyenvs.sh
@@ -1,3 +1,4 @@
+cd ~
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 echo ". ~/miniconda/etc/profile.d/conda.sh" >> ~/.bashrc
