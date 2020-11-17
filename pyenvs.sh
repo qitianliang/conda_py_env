68afbd0eb5820d933e4454fc687017d8bfdfa6c9
qitianliang <qitianliang@outlook.com>
2020-11-17 09:10:15 +0800

    update folder

--- pyenvs.sh
+++ pyenvs.sh
@@ -1,10 +1,11 @@
+# install conda in /home/user folder
 cd ~
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
 rm miniconda.sh
 echo ". ~/miniconda/etc/profile.d/conda.sh" >> ~/.bashrc
 source ~/.bashrc
-cd conda_py_env
+# change directory into envs folder
 for env_file in envs/*.yaml; do
     echo conda create from ${env_file}
     conda env create -f "${env_file}"
@@ -12,7 +13,3 @@ for env_file in envs/*.yaml; do
     conda activate ${env_name}
     python -m ipykernel install --user --name ${env_name} --display-name "${env_name}"
 done
-# conda install -y jupyterlab
-# ip=0.0.0.0
-# port=10001
-# jupyter lab --allow-root --ip ${ip} --port ${port}
